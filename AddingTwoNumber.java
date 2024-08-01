9278385d29ea53b2d36be4f0141f2bddde90a45b
rizwana28 <223T1A05B3@recw.ac.in>
2024-08-01 22:23:43 +0530

    Adding Two Numbers

---
+++ AddingTwoNumber.java
@@ -0,0 +1,14 @@
+package java100days;
+
+import java.util.Scanner;
+
+public class AddingTwoNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int sum=a+b;
+        System.out.println("The Sum is"+ sum);
+    }
+    
+}
